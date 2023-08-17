200c59d312388b153bf00d1ffe32c1a544d8c096
Hemershow <hemersonviolin@gmail.com>
2023-08-16 21:53:42 -0300

    Actually added ball polygons

--- Program.cs
+++ Program.cs
@@ -284,8 +284,6 @@ var lastHexagon = new Hexagon(new Vector3[] {
 
 builder
     .SetCameraStartingPosition(0, 2000, 0)
-    .SetCameraStartingDirection(0, 0, 0)
-    .SetCameraStartingPosition(0, 2000, 0)
     .SetCameraStartingDirection(0, 0 ,0)
     .SetCameraMovementSpeed(5)
     .SetFovAngle(60, 90)
